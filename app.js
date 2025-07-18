14892e88ac11f123b0879faee7d9d1d5379f4f05
ramlen-97 <ramlen97@gmail.com>
2025-07-18 16:50:18 +0530

    read-write-task2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const fs=require("fs");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     //url,method
@@ -9,39 +9,43 @@ const server = http.createServer((req, res) => {
     if (url === '/') {
         //form
         res.setHeader('content-Type', 'text/html');
-        res.end(
-            `
+        fs.readFile("formatValues.txt", (err, data) => {
+            res.end(
+                `
             <form action="/message" method="POST">
                 <label>Name: </label>
                 <input type="text" name="username"></input>
                 <button type="submit">Add</button>
             </form>
+            <h1>${data.toString()}</h1>
             `
-        )
-    }else if(url==='/message'){
-        res.setHeader('content-Type','text/html');
-        const body=[];
-        req.on('data',(chunks)=>body.push(chunks));
-        req.on('end',()=>{
-            let buffer=Buffer.concat(body);
+            )
+        })
+
+    } else if (url === '/message') {
+        res.setHeader('content-Type', 'text/html');
+        const body = [];
+        req.on('data', (chunks) => body.push(chunks));
+        req.on('end', () => {
+            let buffer = Buffer.concat(body);
             console.log(buffer);
 
-            let formData=buffer.toString();
+            let formData = buffer.toString();
             console.log(formData);
 
-            let formValues=formData.split("=")[1];
+            let formValues = formData.split("=")[1];
             console.log(formValues);
 
-            fs.writeFile("formatValues.txt",formValues,(err)=>{
-                res.statusCode=302; // redirected
-                res.setHeader('location','/');
+            fs.writeFile("formatValues.txt", formValues, (err) => {
+                res.statusCode = 302; // redirected
+                res.setHeader('location', '/');
                 res.end();
             });
         })
 
-    }else if(url==="/read"){
+    } else if (url === "/read") {
         // read from the file.
-        fs.readFile('formatValues.txt',(err,data)=>{
+        fs.readFile('formatValues.txt', (err, data) => {
             console.log(data.toString());
 
             res.end(`<h1>${data.toString()}</h1>`);
