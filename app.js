1c337830d466503d1d27d1eabba4534fb53fbf56
ramlen-97 <ramlen97@gmail.com>
2025-07-21 11:15:43 +0530

    handling routes

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express=require('express');
 const app=express();
 
-
-app.use("/welcome",(req,res,next)=>{
-    req.user="Guest";
-    next();
+app.get("/orders",(req,res,next)=>{
+    res.send("Here is the list of all orders")
 })
 
-app.get("/welcome",(req,res,next)=>{
-    res.send(`<h1>Welcome, ${req.user}!</h1>`)
+app.post("/orders",(req,res,next)=>{
+    res.send("A new order has been created");
+})
+
+app.get("/users",(req,res,next)=>{
+    res.send("Here is the list of all users")
+})
+
+app.post("/users",(req,res,next)=>{
+    res.send("A new user has been added");
 })
 
 app.listen(3000,()=>{
-    console.log("Server is running");
+    console.log("Server is running on http://localhost:3000");
 })
