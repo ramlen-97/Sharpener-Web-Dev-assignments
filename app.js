1f79511ec1f9f291cbbc752c0db4992355bce46e
ramlen-97 <ramlen97@gmail.com>
2025-07-21 14:36:59 +0530

    Implementing express routers

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express=require('express');
 const app=express();
 
-app.use((req,res,next)=>{
-    const method=req.method;
-    const url=req.url;
-    console.log(`${method} request made to ${url}`);
-    next();
-})
+const categoryRouter=require('./routes/categories');
+const productRouter=require('./routes/products');
 
-app.get("/products",(req,res,next)=>{
-    res.send("Here is the list of all products")
-})
-
-app.post("/products",(req,res,next)=>{
-    res.send("A new product has been added");
-})
-
-app.get("/categories",(req,res,next)=>{
-    res.send("Here is the list of all categories")
-})
-
-app.post("/categories",(req,res,next)=>{
-    res.send("A new category has been created");
-})
+app.use('/categories',categoryRouter);
+app.use('/products',productRouter);
 
 app.use((req,res)=>{
     res.status(404).send(`<h1>404 - Page not found</h1>`)
