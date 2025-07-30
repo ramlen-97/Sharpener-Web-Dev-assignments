01cdb98141796996381da72ce9e54eaaabe2168b
ramlen-97 <ramlen97@gmail.com>
2025-07-30 12:45:09 +0530

    managing data in DB

--- index.js
+++ index.js
@@ -1,62 +1,17 @@
 const express = require('express');
-const mysql = require('mysql2');
+const db= require('./utils/db-connection');
+const studentRoutes=require('./routes/studentsRoutes')
 const app = express();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Ramlen@97',
-    database: 'busbookingsystem'
-})
+app.use(express.json());
 
-connection.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
 
-    console.log("Connection has been created");
-
-    const creationQuery = [
-        `create table Users(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(225),
-        email VARCHAR(255)
-    )`,
-        `create table Buses(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        busNumber INT,
-        totalSeats INT,
-        availableSeats INT
-    )`,
-        `create table Bookings(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        seatNumber INT
-    )`,
-        `create table Payments(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        amountPaid INT,
-        paymentStatus VARCHAR(255)
-    )`
-
-    ]
-    for(let statement of creationQuery){
-
-        connection.execute(statement, (err) => {
-        if (err) {
-            console.log(err);
-            connection.end();
-            return;
-        }
-        // console.log("Table is created");
-    })
-    }
-    
-})
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("Hello world");
 })
+
+app.use('/students',studentRoutes);
 
 app.listen(3000, (err) => {
     console.log("Server is running");
---
+++ utils/db-connection.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Ramlen@97',
+    database: 'testdb'
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log("Connection has been created");
+
+    const creationQuery = `create table IF NOT EXISTS Students(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(225),
+        email VARCHAR(255)
+    )`
+
+    connection.execute(creationQuery, (err) => {
+        if (err) {
+            console.log(err);
+            connection.end();
+            return;
+        }
+        console.log("Table is created");
+    })
+
+})
+
+module.exports=connection;
